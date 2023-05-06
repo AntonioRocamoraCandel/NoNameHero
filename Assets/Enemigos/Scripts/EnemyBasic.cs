563952d928d629056e68b944e1d839a635f88530
jaimebanos <jaimebanossoriano@gmail.com>
2023-05-06 16:58:56 +0200

    Enemigo gira al moverse en una dirección u otra

--- Assets/Enemigos/Scripts/EnemyBasic.cs
+++ Assets/Enemigos/Scripts/EnemyBasic.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class EnemyBasic : MonoBehaviour
 {
-   
+    public int heal,damage;
     public float velocidadMovimiento = 5f; // Velocidad del movimiento horizontal
     public float maxRange = 5f; // Distancia máxima de movimiento hacia la izquierda
-
-    public float visionDistance = 5f;
 
     private float rightLimit, leftLimit;
     public GameObject target;
@@ -19,11 +17,11 @@ public class EnemyBasic : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // Obtiene el componente Rigidbody2D del sprite
-        rightLimit = transform.position.x + maxRange; // 
-        leftLimit = transform.position.x - maxRange;
+        rightLimit = transform.position.x + maxRange; // Limite de recorrido hacia la derecha
+        leftLimit = transform.position.x - maxRange; // Limite de recorrido hacia la izquierda
         movimiento = Vector2.right * velocidadMovimiento; // Define el movimiento a la derecha como el vector de velocidad por defecto
+        
     }
-
     void FixedUpdate() 
     {
         if(target != null){
@@ -40,15 +38,18 @@ public class EnemyBasic : MonoBehaviour
                 if(transform.position.x <= leftLimit)
                 {
                     movimiento = Vector2.right * velocidadMovimiento;
+                    transform.localScale = new Vector3(1f, 1f, 1f);
                 }
                 // Si el personaje ha llegado al límite derecho, cambia la dirección del movimiento a la izquierda
                 else if(transform.position.x > rightLimit)
                 {
                     movimiento = Vector2.left * velocidadMovimiento;
+                    transform.localScale = new Vector3(-1f, 1f, 1f);
                 }
 
                 // Aplica el movimiento al Rigidbody2D
                 rb.velocity = movimiento;
+
             }
             
         }
