1cd51638643d5ccb5d1620fa3f09fb9c494a30b8
AntonioRocamoraCandel <antoniorocamoracandel@gmail.com>
2023-05-29 21:12:17 +0200

    Destrucción game object roca y colision con Heroe (falta solucionar colisión heroe)

---
+++ Assets/ScriptsEnemigos/Goblin-Range/RockCollision.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+namespace ScriptsEnemigos.Goblin_Range
+{
+    public class RockCollision : MonoBehaviour
+    {
+        public string objectiveTag; 
+        public float timeToDestroy = 3f; 
+
+        private bool collision = false;
+        private float timeElapsed = 0f;
+
+        void Update()
+        {
+            if (!collision)
+            {
+                timeElapsed += Time.deltaTime;
+                if (timeElapsed >= timeToDestroy)
+                {
+                    RockDestroy();
+                }
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(objectiveTag))
+            {
+                Damage(other.gameObject);
+                RockDestroy();
+            }
+        }
+
+        void Damage(GameObject objeto)
+        {
+            // Lógica para hacer daño al objeto con el que colisionó la roca
+            // ...
+        }
+
+        void RockDestroy()
+        {
+            collision = true;
+            Destroy(gameObject);
+        }
+    }
+}
