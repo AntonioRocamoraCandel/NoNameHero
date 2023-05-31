e5c859eb628cece96b423117a8304782526a1d0a
Joaquín Gómez Martínez <joaquingomezmar15@icloud.com>
2023-05-31 12:29:52 +0200

    Implementacion el nuevo input system en el player Prueba, tambien asigne la tecla enter al boton de pasar texto del dialogo de Npc, fue creada la nueva escena del menu aunque sigue en desarrollo

--- Assets/Scripts/MovimientoPlayerBasic.cs
+++ Assets/Scripts/MovimientoPlayerBasic.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class MovimientoPlayerBasic : MonoBehaviour
 {
-    private Rigidbody2D Rigidbody2D;
-    private float Horizontal;
+    public Rigidbody2D Rigidbody2D;
+    private float horizontal;
+
+    private float speed = 3f;
+
+    private bool isFacingRight = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +19,28 @@ public class MovimientoPlayerBasic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");
+        Rigidbody2D.velocity = new Vector2(horizontal * speed, Rigidbody2D.velocity.y);
+
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal <0f){
+            Flip();
+        }
     }
 
-    private void FixedUpdate()
+
+    private void Flip()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal,Rigidbody2D.velocity.y);
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
     }
 }
