f83fc5b4d8ec8038aa3a6ad9b35eb5424d7009a4
Adrian <adrianambene2003@gmail.com>
2023-06-07 23:27:02 +0200

    barra de maná hecha y funcional

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
@@ -41,6 +44,10 @@ public class PlayerMovement : MonoBehaviour
     GameObject bullets;
     GameObject bulletsMagic;
 
+    public Slider visualMana;
+    public float mana;
+    public int costoMana;
+
     private bool canAttack = true; 
     private float attackCooldown = 0.6f; 
     private float lastAttackTime;
@@ -61,12 +68,14 @@ public class PlayerMovement : MonoBehaviour
         extraJumps = 1;
         posicionDisparo=9;
         posicionDisparoMagia=6;
+        StartCoroutine(tiempo());
     }
 
     private void Update()
     {   
         animator.SetFloat("Horizontal", Mathf.Abs(horizontal));
     
+        visualMana.GetComponent<Slider>().value = mana;
 
         if (!isFacingRight && horizontal > 0f)
         {
@@ -101,7 +110,7 @@ public class PlayerMovement : MonoBehaviour
         {
             canAttack = true; 
         }
-        if (!canMagicAttack && Time.time >= lastMagicAttackTime + MagicAttackCooldown)
+        if (!canMagicAttack && Time.time >= lastMagicAttackTime + MagicAttackCooldown && mana >= costoMana)
         {
             canMagicAttack = true; 
         }
@@ -245,6 +254,7 @@ public class PlayerMovement : MonoBehaviour
             animator.SetTrigger("lanzarMagia");
             Invoke("InstantiateBulletMagic", 0.3f);
 
+            mana -=costoMana;
             canMagicAttack = false;
             lastMagicAttackTime = Time.time;
         }
@@ -332,6 +342,17 @@ public class PlayerMovement : MonoBehaviour
                 }
 
                 Invoke(nameof(StopWallJumping), wallJumpingDuration);
+            }
+        }
+    }
+
+    IEnumerator tiempo()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if(mana < 100){
+                mana+=1;
             }
         }
     }
