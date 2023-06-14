dc3aaceed5cfa1bd35ab979d638ba0303887a0e0
Adrian <adrianambene2003@gmail.com>
2023-06-14 12:52:45 +0200

    bola de fuego solucionada

--- Assets/Scripts/Player/MagicDamage.cs
+++ Assets/Scripts/Player/MagicDamage.cs
@@ -23,7 +23,7 @@ public class MagicDamage : MonoBehaviour
         }
 
 
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag(objectiveTag))
             {
